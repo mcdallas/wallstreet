ae970c24e2d9078eff8641ee0c7e722d1d12705f
mcdallas <mcdallas@protonmail.com>
2019-08-10 00:08:26 +0100

    handle Yahoo Finance (lack of) url encoding

--- wallstreet/wallstreet.py
+++ wallstreet/wallstreet.py
@@ -2,8 +2,9 @@ import requests
 import re, json
 
 from datetime import datetime, date, timedelta
-from time import mktime
+from time import mktime, sleep
 from io import StringIO
+from urllib.parse import urlencode
 
 from wallstreet.constants import DATE_FORMAT, DATETIME_FORMAT
 from wallstreet.blackandscholes import riskfree, BlackandScholes
@@ -57,18 +58,20 @@ def strike_required(func):
 class YahooFinanceHistory:
     timeout = 2
     crumb_link = 'https://finance.yahoo.com/quote/{0}/history?p={0}'
-    crumble_regex = r'CrumbStore":{"crumb":"(.*?)"}'
-    quote_link = 'https://query1.finance.yahoo.com/v7/finance/download/{quote}?period1={dfrom}&period2={dto}&interval=1d&events=history&crumb={crumb}'
+    crumb_regex = r'CrumbStore":{"crumb":"(.*?)"}'
+    quote_link = 'https://query1.finance.yahoo.com/v7/finance/download/{quote}'
 
-    def __init__(self, symbol, days_back=7):
+    def __init__(self, symbol, days_back=7, frequency='d'):
         self.symbol = symbol
         self.session = requests.Session()
         self.dt = timedelta(days=days_back)
+        self.frequency = {'m': 'mo', 'w': 'wk', 'd': 'd'}[frequency]
 
     def get_crumb(self):
         response = self.session.get(self.crumb_link.format(self.symbol), timeout=self.timeout)
         response.raise_for_status()
-        match = re.search(self.crumble_regex, response.text)
+        sleep(0.5)
+        match = re.search(self.crumb_regex, response.text)
         if not match:
             raise ValueError('Could not get crumb from Yahoo Finance')
         else:
@@ -78,15 +81,17 @@ class YahooFinanceHistory:
         try:
             import pandas as pd
         except ImportError:
-            print('This functionality requires pandas to be installed')
-            return
+            raise ImportError('This functionality requires pandas to be installed')
+
         if not hasattr(self, 'crumb') or len(self.session.cookies) == 0:
             self.get_crumb()
         now = datetime.utcnow()
         dateto = int(now.timestamp())
         datefrom = int((now - self.dt).timestamp())
-        url = self.quote_link.format(quote=self.symbol, dfrom=datefrom, dto=dateto, crumb=self.crumb)
-        response = self.session.get(url)
+        url = self.quote_link.format(quote=self.symbol)
+        params = {'period1': datefrom, 'period2': dateto, 'interval': f'1{self.frequency}', 'events': 'history', 'crumb': self.crumb}
+        param_string = urlencode(params).replace('%5Cu002F', '/')
+        response = self.session.get(url, params=param_string)
         response.raise_for_status()
         return pd.read_csv(StringIO(response.text), parse_dates=['Date'])
 
@@ -186,9 +191,8 @@ class Stock:
         self.update()
         return self._last_trade.strftime(DATETIME_FORMAT)
 
-    def historical(self, days_back=30):
-
-        return YahooFinanceHistory(symbol=self.ticker, days_back=days_back).get_quote()
+    def historical(self, days_back=30, frequency='d'):
+        return YahooFinanceHistory(symbol=self.ticker, days_back=days_back, frequency=frequency).get_quote()
 
 
 class Option:
