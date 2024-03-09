943a6bf96ac25f0014ec018f10e15dacee229e16
Mike Dallas <mcdallas@protonmail.com>
2024-03-09 22:20:42 +0000

    read version from pyproject.toml

--- wallstreet/__init__.py
+++ wallstreet/__init__.py
@@ -1,18 +1,15 @@
 from wallstreet.wallstreet import Stock, Call, Put
-from pkg_resources import get_distribution, DistributionNotFound
-import os.path
 
 __all__ = ['Stock', 'Call', 'Put']
 
-try:
-    _dist = get_distribution('wallstreet')
-    # Normalize case for Windows systems
-    dist_loc = os.path.normcase(_dist.location)
-    here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(dist_loc, 'wallstreet')):
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
-    __version__ = 'Please install this project with setup.py'
-else:
-    __version__ = _dist.version
+def get_version_from_pyproject():
+    version_line_prefix = 'version ='
+    with open('pyproject.toml', 'r') as file:
+        for line in file:
+            if line.strip().startswith(version_line_prefix):
+                # Extract the version part and remove quotes
+                version = line.split('=')[1].strip().strip('"').strip("'")
+                return version
+    return "unknown"
+
+__version__ = get_version_from_pyproject()
