5b2ec78773cc38aca412d5b3d17035af604c2ade
Mike Dallas <mcdallas@protonmail.com>
2024-03-09 22:26:34 +0000

    hardcode version, bump to 0.4.0

--- wallstreet/__init__.py
+++ wallstreet/__init__.py
@@ -2,14 +2,4 @@ from wallstreet.wallstreet import Stock, Call, Put
 
 __all__ = ['Stock', 'Call', 'Put']
 
-def get_version_from_pyproject():
-    version_line_prefix = 'version ='
-    with open('pyproject.toml', 'r') as file:
-        for line in file:
-            if line.strip().startswith(version_line_prefix):
-                # Extract the version part and remove quotes
-                version = line.split('=')[1].strip().strip('"').strip("'")
-                return version
-    return "unknown"
-
-__version__ = get_version_from_pyproject()
+__version__ = "0.4.0"
